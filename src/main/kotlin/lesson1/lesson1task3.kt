905105f6be64b880f0a6445cf3b269c8e595be0b
MaruikoKruglyy <springspleen@gmail.com>
2024-03-14 20:58:13 +0300

    KS-1-3 strings and interpolation

--- src/main/kotlin/lesson1/lesson1task3.kt
+++ src/main/kotlin/lesson1/lesson1task3.kt
@@ -1,17 +1,14 @@
 package lesson1
 
-fun main () {
+fun main() {
     val year = 1961
-    var hour = 9
-    var minute = 7
+    var hour: String = "09"
+    var minute: String = "07"
     println(year)
     println(hour)
     println(minute)
-    hour = 10
-    minute = 55
-    print (hour)
-    print (":")
-    print (minute)
-
+    hour = "10"
+    minute = "55"
+    println("$hour:$minute")
 
 }
