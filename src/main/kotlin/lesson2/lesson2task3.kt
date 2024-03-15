2dbedc1b930025f65d3f64cbab9f941661675c84
MaruikoKruglyy <springspleen@gmail.com>
2024-03-15 20:37:18 +0300

    KS-2-3 conts val snake case

--- src/main/kotlin/lesson2/lesson2task3.kt
+++ src/main/kotlin/lesson2/lesson2task3.kt
@@ -1,18 +1,18 @@
 package lesson2
 
-const val minutesInHour = 60
+const val MINUTES_IN_HOUR = 60
 
 fun main() {
     val hourOfDeparture = 9
     val minuteOfDeparture = 39
     val travelMinutes = 457
 
-    val hoursOfTravel = travelMinutes / minutesInHour // 7
-    val minutesOfTravelLeft = travelMinutes % minutesInHour // 37
+    val hoursOfTravel = travelMinutes / MINUTES_IN_HOUR // 7
+    val minutesOfTravelLeft = travelMinutes % MINUTES_IN_HOUR // 37
 
     val sumOfMinutes = minuteOfDeparture + minutesOfTravelLeft
-    val minutesLeft = sumOfMinutes % minutesInHour
-    val hoursFromMinutes = sumOfMinutes / minutesInHour
+    val minutesLeft = sumOfMinutes % MINUTES_IN_HOUR
+    val hoursFromMinutes = sumOfMinutes / MINUTES_IN_HOUR
     val sumOfHours = hourOfDeparture + hoursOfTravel + hoursFromMinutes
 
     println("$sumOfHours:$minutesLeft")
