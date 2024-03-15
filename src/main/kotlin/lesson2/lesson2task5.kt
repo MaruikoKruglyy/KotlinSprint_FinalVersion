36849387937b9478bfd6ee397082cb1ab5440ae2
MaruikoKruglyy <springspleen@gmail.com>
2024-03-15 20:39:39 +0300

    KS-2-5 const val snake case

--- src/main/kotlin/lesson2/lesson2task5.kt
+++ src/main/kotlin/lesson2/lesson2task5.kt
@@ -1,14 +1,15 @@
 package lesson2
 
-const val daysPerYear = 365
-const val maximumPercentage = 100
+const val DAYS_PER_YEAR = 365
+const val MAXIMUM_PERCENTAGE = 100
 
 fun main() {
     val initialSum = 70000
     val percentageRate = 16.7
     val numberOfYears = 20.0
 
-    val compoundInterest = (Math.pow((1 + (percentageRate * daysPerYear) / (daysPerYear * maximumPercentage)), numberOfYears)) * initialSum
+    val compoundInterest = (Math.pow((1 + (percentageRate * DAYS_PER_YEAR) / (DAYS_PER_YEAR * MAXIMUM_PERCENTAGE)), numberOfYears)) * initialSum
+
 
     println("%.3f".format(compoundInterest))
 
