f0f994108310760218888e3e42e610cd0fbade13
MaruikoKruglyy <springspleen@gmail.com>
2024-03-15 18:42:00 +0300

    KS-1-5 avoid magic numbers

--- src/main/kotlin/lesson1/lesson1task5.kt
+++ src/main/kotlin/lesson1/lesson1task5.kt
@@ -1,13 +1,16 @@
 package lesson1
 
+const val secondsInAMinute = 60
+const val minutesInAnHour = 60
+
 fun main() {
     val totalSecondsInSpace = 6480
-    val totalMinutesInSpace = totalSecondsInSpace / 60 // 108
-    val fullHoursInSpace = totalMinutesInSpace / 60 // 1
+    val totalMinutesInSpace = totalSecondsInSpace / secondsInAMinute // 108
+    val fullHoursInSpace = totalMinutesInSpace / minutesInAnHour // 1
 
-    val fullHoursInMinutes = fullHoursInSpace * 60 // 60
+    val fullHoursInMinutes = fullHoursInSpace * minutesInAnHour // 60
     val minutesLeft = totalMinutesInSpace - fullHoursInMinutes // 48
-    val secondsLeft = totalSecondsInSpace % 60 // 00
+    val secondsLeft = totalSecondsInSpace % secondsInAMinute // 00
 
     println("%02d:%02d:%02d".format(fullHoursInSpace, minutesLeft, secondsLeft))
 
