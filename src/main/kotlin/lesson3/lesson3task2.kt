453611b0e0550cba9f9933440a0fc605247b4b43
MaruikoKruglyy <springspleen@gmail.com>
2024-03-16 18:35:55 +0300

    KS-3-2 create names

---
+++ src/main/kotlin/lesson3/lesson3task2.kt
@@ -0,0 +1,13 @@
+package lesson3
+
+fun main() {
+    val birthSurname = "Андреева"
+    val marriedSurname = "Сидорова"
+    val name = "Татьяна"
+    val patronymicName = "Сергеевна"
+    val ageBeforeMarriage = 20
+    val ageOfMarriage = 22
+
+    println("[$birthSurname $name $patronymicName, $ageBeforeMarriage]")
+    println("[$marriedSurname $name $patronymicName, $ageOfMarriage]")
+}
