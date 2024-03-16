adf871c2b6bfa7c748497e0976cc71463048a124
MaruikoKruglyy <springspleen@gmail.com>
2024-03-16 18:34:40 +0300

    KS-3-1 create greetings

---
+++ src/main/kotlin/lesson3/lesson3task1.kt
@@ -0,0 +1,9 @@
+package lesson3
+
+fun main() {
+    val userName = "Александра"
+    var greeting = "доброго дня!"
+    println("$userName, $greeting")
+    greeting = "доброго вечера!"
+    println("$userName, $greeting")
+}
