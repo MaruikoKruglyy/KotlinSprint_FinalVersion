b8a3e1ac171da1cf4e9b65221f43cc179343a837
MaruikoKruglyy <160403381+MaruikoKruglyy@users.noreply.github.com>
2024-03-17 22:39:12 +0300

    Delete src/main/kotlin/lesson3/lesson3task1.kt

--- src/main/kotlin/lesson3/lesson3task1.kt
+++
@@ -1,9 +0,0 @@
-package lesson3
-
-fun main() {
-    val userName = "Александра"
-    var greeting = "доброго дня!"
-    println("$userName, $greeting")
-    greeting = "доброго вечера!"
-    println("$userName, $greeting")
-}
