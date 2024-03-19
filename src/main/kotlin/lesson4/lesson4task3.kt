ef23f0ddc803d6f1ea4c541799d28711140db978
MaruikoKruglyy <springspleen@gmail.com>
2024-03-19 19:46:12 +0300

    KS-4-3 change const val to Boolean

--- src/main/kotlin/lesson4/lesson4task3.kt
+++ src/main/kotlin/lesson4/lesson4task3.kt
@@ -1,13 +1,13 @@
 package lesson4
 
-const val BENEFICIAL_WEATHER = "солнечно"
-const val BENEFICIAL_TENT = "открыт"
+const val IS_SUNNY = true
+const val IS_AWNING_OPEN = true
 const val BENEFICIAL_HUMIDITY_PERCENTAGE = 20
 const val ADVERSE_SEASON = "зима"
 
 fun main() {
-    val weatherNow = "солнечно" == BENEFICIAL_WEATHER
-    val tentNow = "открыт" == BENEFICIAL_TENT
+    val weatherNow = IS_SUNNY
+    val tentNow = IS_AWNING_OPEN
     val humidityNow = 20
     val seasonNow = "зима"
 
