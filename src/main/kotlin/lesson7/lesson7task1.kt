0916c0079d63791e0a5b8d582052ebef82ca26f3
MaruikoKruglyy <springspleen@gmail.com>
2024-03-22 00:42:48 +0300

    KS-7-1 random password

---
+++ src/main/kotlin/lesson7/lesson7task1.kt
@@ -0,0 +1,13 @@
+package lesson7
+
+fun main() {
+
+    val numberOne = (1..9).random()
+    val numberTwo = ('a'..'z').random()
+    val numberThree = (1..9).random()
+    val numberFour = ('a'..'z').random()
+    val numberFive = (1..9).random()
+    val numberSix = ('a'..'z').random()
+
+    println("$numberOne$numberTwo$numberThree$numberFour$numberFive$numberSix")
+}
