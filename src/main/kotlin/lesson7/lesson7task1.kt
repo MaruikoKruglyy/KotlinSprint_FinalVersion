3cf1ff58680567ec76e0ce763ae09710d68e56dd
MaruikoKruglyy <springspleen@gmail.com>
2024-03-22 21:34:06 +0300

    KS-7-1 modified

--- src/main/kotlin/lesson7/lesson7task1.kt
+++ src/main/kotlin/lesson7/lesson7task1.kt
@@ -2,12 +2,9 @@ package lesson7
 
 fun main() {
 
-    val numberOne = (1..9).random()
-    val numberTwo = ('a'..'z').random()
-    val numberThree = (1..9).random()
-    val numberFour = ('a'..'z').random()
-    val numberFive = (1..9).random()
-    val numberSix = ('a'..'z').random()
-
-    println("$numberOne$numberTwo$numberThree$numberFour$numberFive$numberSix")
+    for (i in 1..3) {
+        val numberOne = (1..9).random()
+        val numberTwo = ('a'..'z').random()
+        print("$numberOne$numberTwo")
+    }
 }
