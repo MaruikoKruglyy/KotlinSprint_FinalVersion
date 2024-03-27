48edd70d6cfb157b39acfd349a33d91fb6927fd5
MaruikoKruglyy <springspleen@gmail.com>
2024-03-28 01:25:39 +0300

    KS-8-4 change name of val,remove cycle

--- src/main/kotlin/lesson8/lesson8task4.kt
+++ src/main/kotlin/lesson8/lesson8task4.kt
@@ -4,30 +4,19 @@ fun main() {
 
     val arrayOfIngredients = arrayOf("картофель", "соль", "масло", "зелень", "лук")
     println("Список ингредиентов:")
-    for (i in arrayOfIngredients) {
-        if (i == "лук") {
-            print("$i. ")
-        } else print("$i, ")
-    }
+    println(arrayOfIngredients.joinToString(", ", "", "."))
 
     println("Какой ингредиент нужно заменить?")
     val ingredientToChange = readln()
 
-    for (a in arrayOfIngredients) {
-        if (a == ingredientToChange) {
-            println("Какой ингредиент вы хотите добавить вместо?")
-            val newIngredient = readln()
-            val index = arrayOfIngredients.indexOf(a)
-            arrayOfIngredients.set(index, newIngredient)
-            println("Готово! Вы сохранили следующий список:")
-            for (b in arrayOfIngredients) {
-                if (b == arrayOfIngredients[4]) {
-                    print("$b. ")
-                } else print("$b, ")
-            }
-            return
-        }
-
+    if (ingredientToChange in arrayOfIngredients) {
+        println("Какой ингредиент вы хотите добавить вместо?")
+        val newIngredient = readln()
+        val index = arrayOfIngredients.indexOf(ingredientToChange)
+        arrayOfIngredients[index] = newIngredient
+        println("Готово! Вы сохранили следующий список:")
+        println(arrayOfIngredients.joinToString(", ", "", "."))
+        return
     }
     println("Данного ингредиента в списке нет!")
 }
