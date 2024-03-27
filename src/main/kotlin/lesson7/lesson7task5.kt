037a212ea7e8ebdb9258d5d58cc59955e54030ba
MaruikoKruglyy <springspleen@gmail.com>
2024-03-27 21:31:05 +0300

    KS-7-5

--- src/main/kotlin/lesson7/lesson7task5.kt
+++ src/main/kotlin/lesson7/lesson7task5.kt
@@ -1,21 +1,25 @@
 package lesson7
 
 fun main() {
-
-
     println("Введите длину пароля, минимальная длина 6 символов:")
     var passwordLength = readln().toInt()
     while (passwordLength < 6) {
         println("Ведите число не меньше шести!")
         passwordLength = readln().toInt()
-
     }
-    val password = mutableListOf((1..9).random(), ('a'..'z').random(), ('A'..'Z').random())
-for (i in password) {
-    print(i)
-}
-for (i in 4..passwordLength) {
+
+    val numbers = '1'..'9'
+    val upperCase = 'A'..'Z'
+    val lowerCase = 'a'..'z'
+
+    val allChars = numbers + upperCase + lowerCase
+    val password: MutableList<Char> = mutableListOf()
+    password.add(numbers.random())
+    password.add(upperCase.random())
+    password.add(lowerCase.random())
+    for (i in 4..passwordLength) {
+        password.add(allChars.random())
+    }
     password.shuffle()
-      print(password.random())
-}
+    println(password.joinToString(""))
 }
